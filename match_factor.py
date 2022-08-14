2184d9844a2cb8e26ef46d18ab09901f900eec1c
louis-stp <ljs329@cornell.edu>
2022-08-13 22:33:33 -0400

    Developed the optimize function further, started defining objective variables and constraints

--- match_factor.py
+++ match_factor.py
@@ -1,5 +1,5 @@
+from ortools.linear_solver import pywraplp
 import numpy as np
-from scipy.optimize import linprog
 
 #this class defines a type of truck which have a certain name and capacity
 class TruckType:
@@ -33,6 +33,62 @@ class Fleet:
         self.loaders += [loader]
 
     def optimize(self):
-        pass
+        lp = pywraplp.Solver.CreateSolver('SCIP')
+        infinity = lp.infinity()
+        numLoaders = len(self.loaders)
+        numTruckTypes = len(self.truckFleet)
+
+        A = makeA()
+        
+        #adds the assignment variables to the integer problem
+        #These define how many of truck type i is assigned to loader j
+        x = []
+        for i in range(numTruckTypes):
+            for j in range(numLoaders):
+                x += [lp.IntVar(0, infinity, 'x_'+str(i)+'_'+str(j))]
+
+        #Adds the delta variables. These variables measure the absolute value of
+        #the match factor of each loader from the desired match factor (default 1).
+        d = []
+        for j in range(numLoaders):
+            d += [lp.IntVar(0,infinity, 'd_'+str(j))]
+
+        #constrains the total number of each type of truck 
+        for i,truck in enumerate(self.truckFleet):
+            maxTrucks = list(truck.values())[0]
+            constraint = lp.RowConstraint(0, maxTrucks, '')
+            #sums the number of trucks over all loaders. Hence coeff = 1 for relevant coeffs and 0 otherwise
+            for k in range(len(x)):
+                
+
+        #sets the delta constraints
+        for l in range(len(d)):
+            constraint = lp.RowConstraint(0, 1, '')
+
+    def makeA(self):
+        numLoaders = len(self.loaders)
+        numTruckTypes = len(self.truckFleet)
+        numVar = numLoaders+numLoaders*numTruckTypes
+        A = np.zeros([numVar,numVar])
+
+        for i in range(numTruckTypes):
+            for k in range(numVar):
+                if(k >= i*numTruckTypes)
+                A[]
+        
+        #make truck model number constraint rows
+        
+
+
+                
+
+            
+
+fleet = Fleet()
+fleet.addTrucks(TruckType("CAT797",400), 5)
+fleet.addTrucks(TruckType("CAT777",100), 4)
+fleet.addLoader(Loader("Bucyrus_1",55,.25,1,2000))
+fleet.addLoader(Loader("Bucyrus_2",55,.25,1,1500))
+fleet.optimize()
 
     
